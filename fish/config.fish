85769ba01a5b357d43076bd1b03dad5e53e87883
Garrett Phelps <ga16561@fmh.com>
2026-05-15 14:32:04 -0500

    Fix tmux issues

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,6 @@ export PATH="/opt/homebrew/bin:$PATH"
 starship init fish | source
 if status is-interactive
     and not set -q TMUX
-    exec tmux new -s Default
+    tmux attach -t Default 2>/dev/null; or exec tmux new -s Default
 end
+export PATH="$HOME/.local/bin:$PATH"
